1afdbd1869de6d47c1951c68c4eca6aeec2275f9
Zaher Dirkey <zaherdirkey@yahoo.com>
2016-07-18 23:05:04 +0300

    Fixed repeating Keycode Use UsbKeyboard as namespace

--- usbIRConsumer.ino
+++ usbIRConsumer.ino
@@ -11,6 +11,7 @@
 
 //#define DEBUG //enable it to send out info over Serial, not work in attiny
 
+#include <avr/pgmspace.h>
 #include <UsbKeyboard.h>
 #include <miniIRemote.h>
 
@@ -21,70 +22,82 @@
 */
 
 void sendNumber(uint32_t n) {
-  char str[40];
+  char str[12];
   ultoa(n, str, 10);
-  UsbKeyboard.sendString(str);
-  UsbKeyboard.wait();
+  UsbKeyboard::sendString(str);
+  UsbKeyboard::wait();
 }
 
 void setup() {
   TIMSK &= !(1 << TOIE0); // Disable timer0 as it can interfere with USB timing
+  UsbKeyboard::init();
   miniIR::init(0);  
 }
 
-uint32_t irLastCode = 0;
+void loop() {
+    static uint32_t irLastCode = 0;
+    static uint8_t irLastCount = 0;
 
-void loop() {   
-    UsbKeyboard.delay();
-    UsbKeyboard.update();
+    UsbKeyboard::delay();
+    UsbKeyboard::update();
     
     uint32_t irCode = miniIR::Read(); // Wait for an IR Code        
     
-    UsbKeyboard.update(); // needs to be called often    
+    UsbKeyboard::update(); // needs to be called often    
+    
     if (irCode == 1) {
-      irCode = irLastCode;
+      irLastCount++;
+      if (irLastCount < 2) { //drop first few repeats, to slowing send data
+        irCode = 0;
+        UsbKeyboard::delay();
+        UsbKeyboard::update();
+      }
+      else{        
+        irLastCount = 0;
+        irCode = irLastCode;  
+      }          
+    }
+    else {
+      irLastCount = 0;
     }
     
     if (irCode > 0) {
       irLastCode = irCode;
-      if (irCode == 1086263535) { // Single character example, "1"
-        UsbKeyboard.sendKeyStroke(KEY_1);
+      if (irCode == 1086263535) { // Single character "1"
+        UsbKeyboard::sendKeyStroke(KEY_1);
       } else if (irCode == 1086296175) {
-        UsbKeyboard.sendKeyStroke(KEY_2);    
+        UsbKeyboard::sendKeyStroke(KEY_2);    
       } else if (irCode == 1086279855) {
-        UsbKeyboard.sendKeyStroke(KEY_3);    
+        UsbKeyboard::sendKeyStroke(KEY_3);    
       } else if (irCode == 1086271695) {
-        UsbKeyboard.sendKeyStroke(KEY_4);    
+        UsbKeyboard::sendKeyStroke(KEY_4);    
       } else if (irCode == 1086304335) {
-        UsbKeyboard.sendKeyStroke(KEY_5);    
+        UsbKeyboard::sendKeyStroke(KEY_5);    
       } else if (irCode == 1086288015) {
-        UsbKeyboard.sendKeyStroke(KEY_6);    
+        UsbKeyboard::sendKeyStroke(KEY_6);    
       } else if (irCode == 1086261495) {
-        UsbKeyboard.sendKeyStroke(KEY_7);    
+        UsbKeyboard::sendKeyStroke(KEY_7);    
       } else if (irCode == 1086294135) {
-        UsbKeyboard.sendKeyStroke(KEY_8);    
+        UsbKeyboard::sendKeyStroke(KEY_8);    
       } else if (irCode == 1086277815) {
-        UsbKeyboard.sendKeyStroke(KEY_9);    
+        UsbKeyboard::sendKeyStroke(KEY_9);    
       } else if (irCode == 1086269655) {
-        UsbKeyboard.sendKeyStroke(KEY_0);    
+        UsbKeyboard::sendKeyStroke(KEY_0);    
       } else if (irCode == 1086259455) {
-        UsbKeyboard.sendConsumerStroke(CONSUMER_MUTE);
+        UsbKeyboard::sendConsumerStroke(CONSUMER_MUTE);
       } else if (irCode == 1086320655) {
-        UsbKeyboard.sendConsumerStroke(CONSUMER_CHANNEL_INCREMENT);
+        UsbKeyboard::sendConsumerStroke(CONSUMER_CHANNEL_INCREMENT);
       } else if (irCode == 1086310455) {
-        UsbKeyboard.sendConsumerStroke(CONSUMER_CHANNEL_DECREMENT);
+        UsbKeyboard::sendConsumerStroke(CONSUMER_CHANNEL_DECREMENT);
       } else if (irCode == 1086316575) {
-        UsbKeyboard.sendConsumerStroke(CONSUMER_VOLUME_INCREMENT);
+        UsbKeyboard::sendConsumerStroke(CONSUMER_VOLUME_INCREMENT);
       } else if (irCode == 1086283935) {
-        UsbKeyboard.sendConsumerStroke(CONSUMER_VOLUME_DECREMENT);
+        UsbKeyboard::sendConsumerStroke(CONSUMER_VOLUME_DECREMENT);
       }
-      else {
-        /*
-        UsbKeyboard.sendString(" Code: ");      
-        UsbKeyboard.update();
+      else {        
+        /*UsbKeyboard::sendString(" Code: ");
+        UsbKeyboard::update();
         sendNumber(irCode);*/
       }
-    }
-    UsbKeyboard.delay(); 
+    }    
 }
-
